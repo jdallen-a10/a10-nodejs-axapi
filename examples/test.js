4cfcc9ad2c5c6dd5e875e1ff0e6b6ad39b1820d6
John D. Allen <jallen@a10networks.com>
2020-07-29 19:45:24 -0500

    Added example program

---
+++ examples/test.js
@@ -0,0 +1,39 @@
+
+var A10 = require('a10-nodejs-axapi');
+Object.assign(A10.prototype, require('./node_modules/a10-nodejs-axapi/lib/a10_system.js'));
+
+var opts = {
+  DEBUG: 10,
+  IPAddr: "44.147.45.201",
+  User: "admin",
+  Passwd: "a10"
+}
+
+var th = new A10(opts);
+
+th.init().then((out) => {
+  th.getHostname().then((hn) => {
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + hn);
+    console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + th.version.version);
+    console.log(">>>>>>>>" + JSON.stringify(th, null, "  "));
+    th.getUptime().then((out) => {
+      console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + out);
+      th.getPlatform().then((out) => {
+        console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + out);
+        th.logoff().then((out) => {
+          console.log(out);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }).catch((err) => {
+    console.log(err);
+  });
+}).catch((err) => {
+  console.log(err);
+});
